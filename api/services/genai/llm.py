64771ad3bc6993ff723620487cac5a0910492082
Shubhankar Agrawal <shubhankar.a31@gmail.com>
2025-11-26 21:05:08 +0800

    Move to 2.5 Flash Gemini

--- api/services/genai/llm.py
+++ api/services/genai/llm.py
@@ -11,7 +11,7 @@ import time
 import os
 
 genai.configure(api_key=os.environ["LLM_API_KEY"])
-model = genai.GenerativeModel('gemini-3-pro-image-preview') 
+model = genai.GenerativeModel('gemini-2.5-flash-image') 
 
 llm = Blueprint('llm', __name__)
 
