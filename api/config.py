fff51ac5c280b3049c5d8f6b5aee8a6b31e67924
Shubhankar Agrawal <shubhankar.a31@gmail.com>
2026-02-07 16:30:08 +0800

    Updte Redis Config

--- api/config.py
+++ api/config.py
@@ -11,8 +11,8 @@ class MetaConfig:
     IG_GRAPH_API_URL = 'https://graph.instagram.com'
 
 class RedisConfig:
-    HOST = 'redis-12079.c8.us-east-1-3.ec2.redns.redis-cloud.com'
-    PORT = 12079
+    HOST = 'redis-18037.c283.us-east-1-4.ec2.cloud.redislabs.com'
+    PORT = 18037
     PASS = os.getenv('REDIS_PASS', '')
     
 class LLMConfig:    
